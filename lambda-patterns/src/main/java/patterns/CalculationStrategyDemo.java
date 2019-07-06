5145c7d8ab2b460d2158c3e8ed6d10cb8c3d7b0f
Dzmitry Maridau <vinmaster@tut.by>
2019-07-06 17:29:14 +0300

    Lambda patterns: Lightweight calculation strategy

---
+++ lambda-patterns/src/main/java/patterns/CalculationStrategyDemo.java
@@ -0,0 +1,27 @@
+package patterns;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CalculationStrategyDemo {
+    public static void main(String[] args) {
+        List<Integer> numbers = IntStream
+                .generate(() -> (int) (Math.random() * 100))
+                .limit(10)
+                .boxed()
+                .collect(Collectors.toList());
+
+        System.out.printf("Total: %d%n", calculate(numbers, number -> true));
+        System.out.printf("Sum of odd numbers: %d%n", calculate(numbers, number -> number % 2 == 0));
+        System.out.printf("Sum numbers of even numbers: %d%n", calculate(numbers, number -> number % 2 != 0));
+    }
+
+    private static long calculate(List<Integer> numbers, Predicate<Integer> selector) {
+        return numbers.stream()
+                .filter(selector)
+                .mapToLong(longNum -> longNum)
+                .sum();
+    }
+}
