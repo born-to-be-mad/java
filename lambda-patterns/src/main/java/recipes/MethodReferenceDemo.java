9dde6c51cdc0950a5f7ace0202b590d969663696
Dzmitry Maridau <vinmaster@tut.by>
2019-07-06 16:35:11 +0300

    Stream basics. MethodReferenceDemo.

--- lambda-patterns/src/main/java/recipes/MethodReferenceDemo.java
+++ lambda-patterns/src/main/java/recipes/MethodReferenceDemo.java
@@ -1,6 +1,7 @@
 package recipes;
 
 import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
 
 public class MethodReferenceDemo {
     public static void main(String[] args) {
@@ -9,6 +10,12 @@ public class MethodReferenceDemo {
                 .generate(Math::random) //infinitive stream
                 .limit(20) //ensure that only 20 elements produced
                 .forEach(System.out::println);
+
+        System.out.println("Invoking the length method of class String using a method reference");
+        Stream.of("This", "is", "very", "simple", "stream", "of", "strings")
+                .map(String::length)
+                .forEach(System.out::print);
+
 
     }
 }
