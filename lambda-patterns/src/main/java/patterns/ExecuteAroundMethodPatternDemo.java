feb4e98fdcc611017f13beb07e0895347080f823
Dzmitry Maridau <vinmaster@tut.by>
2019-07-13 10:45:42 +0300

    Execute Around Method Pattern: basic demo with unclosable resource.

---
+++ lambda-patterns/src/main/java/patterns/ExecuteAroundMethodPatternDemo.java
@@ -0,0 +1,34 @@
+package patterns;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class ExecuteAroundMethodPatternDemo {
+    public static void main(String[] args) {
+        Resource resource = new Resource();
+        resource.simpleOperation();
+        resource.complexOperation();
+        //no "close resources"
+    }
+}
+
+class Resource implements Closeable {
+    Resource() {
+        System.out.println("Resource created...");
+    }
+
+    Resource simpleOperation() {
+        System.out.println("simple operation");
+        return this;
+    }
+
+    Resource complexOperation() {
+        System.out.println("complex operation");
+        return this;
+    }
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("close resources");
+    }
+}
