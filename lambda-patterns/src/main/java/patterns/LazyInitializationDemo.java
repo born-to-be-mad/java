743087ad43c9626ccbbe4cfb6c87aa907bcaaa58
Dzmitry Maridau <vinmaster@tut.by>
2019-07-13 09:46:46 +0300

    Lazy initialization through Supplier-function.

--- lambda-patterns/src/main/java/patterns/LazyInitializationDemo.java
+++ lambda-patterns/src/main/java/patterns/LazyInitializationDemo.java
@@ -4,15 +4,15 @@ import java.util.function.Supplier;
 
 public class LazyInitializationDemo {
     public static void main(String[] args) {
-        final Lazy<Integer> lazy = new Lazy<>(() -> computeValue(100));
-
+        final Lazy<Integer> lazyA = new Lazy<>(() -> computeValue(100));
+        final Lazy<Integer> lazyB = new Lazy<>(() -> computeValue(10));
         int a = 1;
         int b = -1;
         System.out.println("Start");
-        if (a > 0 && lazy.getInstance() > 5) {
+        if (a > 0 && lazyA.getInstance() > 5) {
             System.out.println("Path1...");
         }
-        if (b > 0 && lazy.getInstance() > 5) {
+        if (b > 0 && lazyB.getInstance() > 5) {
             System.out.println("Path2...");
         }
         System.out.println("End");
