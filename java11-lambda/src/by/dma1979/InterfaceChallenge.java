e54b6467db8884147ba197a108335033ecb1ec5f
Dzmitry Marudau <d.marudau@sam-solutions.com>
2019-08-14 16:42:39 +0300

    InterfaceChallenge(Java>=9).

---
+++ java11-lambda/src/by/dma1979/InterfaceChallenge.java
@@ -0,0 +1,44 @@
+package by.dma1979;
+
+import java.util.function.Function;
+
+/**
+ * @author dzmitry.marudau
+ * @since 2019.08
+ */
+public class InterfaceChallenge {
+
+    interface Jedi {
+        String MASTER = "Yoda";
+
+        default String attack() {
+            return jump(jedi -> String.join(jedi, useSaber(), useForce()));
+        }
+
+        private String jump(Function<String, String> function) {
+            return function.apply("Luke");
+        }
+
+        private static String useSaber() {
+            return "S";
+        }
+
+        private String useForce() {
+            return "F";
+        }
+
+    }
+
+    public static void main(String... starWars) {
+        System.out.println(new Jedi() {
+            public String useForce() {
+                return "X";
+            }
+        }.attack() + Jedi.useSaber() + Jedi.MASTER);
+        // first instruction returns 'S'+'Luke'+'F' where 'Luke' is delimiter between 'S', 'F' passed as the parameters
+        // Jedi.useSaber() returns 'S'
+        // Jedi.MASTER returns 'Yoda'
+
+        //Final result: SLukeFSYoda
+    }
+}
