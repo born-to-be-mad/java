1add2b7fbcaa60f9cb774ce0b8d3019b8325aa9c
Dzmitry Marudau <vinmaster@tut.by>
2019-08-29 17:52:44 +0300

    Working with Streams in Java: once the Stream is used at the intStream.min() invocation, the same Stream can't be used again to the intStream2.max() method invocation.

---
+++ java11-lambda/src/by/dma1979/challendges/MinMaxChallenge.java
@@ -0,0 +1,28 @@
+package by.dma1979.challendges;
+
+import java.util.List;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+/**
+ * @author dzmitry.marudau
+ * @since 2019.8
+ */
+public class MinMaxChallenge {
+
+    public static void main(String... doYourBest) {
+        IntStream intStream = List.of(1, 2, 3, 4, 5, 6)
+            .stream().mapToInt(n -> n);
+
+        IntStream intStream2 = intStream;
+
+        OptionalInt optIntMin = intStream.min();
+        //once the Stream is used at the intStream.min() invocation, the same Stream can't be used again to the intStream2.max() method invocation.
+        OptionalInt optIntMax = intStream2.max();
+
+        int sum = optIntMax.orElse(5) + optIntMin.orElse(5);
+
+        System.out.println(sum);
+    }
+
+}
