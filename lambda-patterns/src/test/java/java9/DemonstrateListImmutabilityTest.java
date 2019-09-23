9810516f5bfb90392fe15b3db87440422267cbf5
Dzmitry Maridau <vinmaster@tut.by>
2019-09-24 00:21:56 +0300

    The static factory methods List.of , Set.of , and Map.of  are available in Java 9

--- lambda-patterns/src/test/java/java9/DemonstrateListImmutabilityTest.java
+++ lambda-patterns/src/test/java/java9/DemonstrateListImmutabilityTest.java
@@ -10,7 +10,7 @@ import java.util.List;
  * @created at : 00:06
  * @since : 2019.09
  **/
-public class DemonstrateImmutabilityTests {
+public class DemonstrateListImmutabilityTest {
     @Test
     public void showImmutabilityAdd() {
         Assertions.assertThrows(UnsupportedOperationException.class, () -> {
