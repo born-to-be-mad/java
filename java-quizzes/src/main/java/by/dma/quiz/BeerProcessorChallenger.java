52c37e9e4233322b16d90b828eb1a83230f6d5ab
Dzmitry Marudau <vinmaster@tut.by>
2020-03-29 15:00:12 +0300

    Quiz on Lambda

---
+++ java-quizzes/src/main/java/by/dma/quiz/BeerProcessorChallenger.java
@@ -0,0 +1,26 @@
+package by.dma.quiz;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Quiz on Lambda. Absorb the powerful concepts of functional programming with lambdas by solving this Java quiz!
+ * Remember that Lambdas are lazy!
+ *
+ * @author dzmitry.marudau
+ * @since 2020.2
+ */
+public class BeerProcessorChallenger {
+    private static int drunkenness;
+
+    public static void main(String... doYourBest) {
+        Supplier<Integer> moeBeerSupplier = () -> drunkenness = 5; // Line 10
+        drunkenness = 10;
+        Function<Integer, Integer> processBeer = beerProcessor -> drunkenness = (beerProcessor + 2);
+
+        Consumer<Integer> homerBeerConsumer = System.out::println;
+        homerBeerConsumer.accept(moeBeerSupplier.get() + processBeer.apply(drunkenness)); // Line 15
+    }
+
+}
