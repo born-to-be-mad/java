242e29c6044ad61eb94ab69b15171efaf11bab44
Dzmitry Marudau <dzmitry.marudau@unycom.com>
2020-04-06 19:36:55 +0300

    Add missed fake Utils

---
+++ commons/src/main/java/by/dma/commons/Utils.java
@@ -0,0 +1,13 @@
+package by.dma.commons;
+
+public class Utils {
+
+    public static String getMessage() {
+        return "Hello!";
+    }
+
+    public static void doStuff() {
+        System.out.println("doing stuff...");
+    }
+
+}
