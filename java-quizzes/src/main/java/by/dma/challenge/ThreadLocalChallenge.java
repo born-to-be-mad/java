ba2ce8d55c8ca9fb861d740cab7749c3489c1341
Dzmitry Marudau <vinmaster@tut.by>
2020-04-20 20:27:22 +0300

    Quiz on Threads.

---
+++ java-quizzes/src/main/java/by/dma/challenge/ThreadLocalChallenge.java
@@ -0,0 +1,43 @@
+package by.dma.challenge;
+
+/**
+ * Quiz on Threads.
+ * You will learn how to store data isolated by different Threads to avoid collision of data.
+ *
+ * @author dzmitry.marudau
+ * @since 2020.3
+ */
+public class ThreadLocalChallenge {
+    public static void main(String... doYourBest) throws InterruptedException {
+        MarvelThreadLocal wolverine = new MarvelThreadLocal("Wolverine", true);
+        startJoinThread(wolverine);
+        System.out.println(wolverine.xmanName.get());
+
+        startJoinThread(new MarvelThreadLocal("Xavier", true));
+        startJoinThread(new MarvelThreadLocal("Cyclops", false));
+    }
+
+    private static void startJoinThread(MarvelThreadLocal marvelThreadLocal) throws InterruptedException {
+        marvelThreadLocal.start();
+        marvelThreadLocal.join();
+    }
+
+    static class MarvelThreadLocal extends Thread {
+        ThreadLocal<String> xmanName = new ThreadLocal<>();
+        boolean canBeatMagneto;
+
+        MarvelThreadLocal(String name, boolean canBeatMagneto) {
+            xmanName.set(name);
+            this.canBeatMagneto = canBeatMagneto;
+        }
+
+        public void run() {
+            if (canBeatMagneto) {
+                System.out.println(xmanName.get());
+            } else {
+                System.out.println("Magneto");
+            }
+        }
+
+    }
+}
