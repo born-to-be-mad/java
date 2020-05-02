74f2a345a61d46d92066433d41374975cf3b7d93
Dzmitry Marudau <vinmaster@tut.by>
2020-05-02 15:57:38 +0300

    Quiz on Casting: how to manipulate Java types in a deeper way by solving this quiz.

---
+++ java-quizzes/src/main/java/by/dma/challenge/CastingChallenge.java
@@ -0,0 +1,26 @@
+package by.dma.challenge;
+
+/**
+ * Quiz on Casting: how to manipulate Java types in a deeper way by solving this quiz.
+ *
+ * @author dzmitry.marudau
+ * @since 2020.3
+ */
+public class CastingChallenge {
+    public static void main(String... doYourBest) {
+        int number1 = (int) (long) 1.0D;
+        short number2 = 1;
+        Short number3 = (Short) (short) (float) 1.0D;
+        Object number4 = (int) (Number) (Double) (Object) (double) 2.0D;
+        Double number5 = (double) (int) (char) 2.0D;
+        Short number6 = getNumber(Short.valueOf("2"));
+
+        System.out.println(number1 + number2 + number3.doubleValue()
+                           + Integer.valueOf(number4.toString()) + number5.doubleValue()
+                           + number6.doubleValue());
+    }
+
+    static <T> T getNumber(T number) {
+        return number;
+    }
+}
