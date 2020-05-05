bf1608fa9e8cbb8ab6e2d6ae666349f908570ff8
Dzmitry Marudau <vinmaster@tut.by>
2020-05-05 16:02:58 +0300

    Quiz on Types, Values and Variables: give you deep understanding of using double/float values in java.

---
+++ java-quizzes/src/main/java/by/dma/challenge/DevGymJavaBasic.java
@@ -0,0 +1,14 @@
+package by.dma.challenge;
+
+/**
+ * Quiz on Types, Values and Variables: give you deep understanding of using double/float values in java.
+ *
+ * @author dzmitry.marudau
+ * @since 2020.3
+ */
+public class DevGymJavaBasic {
+    public static void main(String[] args) {
+        double d = 1.0 / 0;
+        System.out.println(d);
+    }
+}
