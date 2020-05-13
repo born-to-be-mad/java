3705eba716cb6971121c68f7d443bed9f75a7bc8
Dzmitry Marudau <vinmaster@tut.by>
2020-05-13 18:35:04 +0300

    Quiz on Garbage Collection: how many objects will be eligible for the garbage collector after the executeAction() invocation?

---
+++ java-quizzes/src/main/java/by/dma/challenge/java6/GarbageCollectorTest.java
@@ -0,0 +1,63 @@
+package by.dma.challenge.java6;
+
+/**
+ * How many objects will be eligible for the garbage collector after the executeAction() invocation?
+ *
+ * @author dzmitry.marudau
+ * @since 2020.3
+ */
+public class GarbageCollectorTest {
+    public static void main(String[] args) {
+
+        HowManyObjectsWillBeCleanedByTheGarbageCollector gc = new GarbageCollectorTest()
+            .new HowManyObjectsWillBeCleanedByTheGarbageCollector();
+
+        gc.executeAction();
+
+        System.out.println("How many objects are eligible be collected by "
+                           + "the Garbage Collector after "
+                           + "the executeAction() method invokation?" + gc);
+
+    }
+
+    private class HowManyObjectsWillBeCleanedByTheGarbageCollector {
+
+        private Character homer = new Character("Dooh");
+
+        private Character misterBurns = new Character("you are fired");
+
+        private Character bart = new Character("eat my shorts");
+
+        private Character elBarto;
+
+        public void executeAction() {
+            homer.talk();
+            misterBurns.talk();
+            makeTwoCharactersTalk(misterBurns, homer);
+            misterBurns = null;
+            elBarto = bart;
+            bart = null;
+            elBarto.talk();
+        }
+
+        private void makeTwoCharactersTalk(Character misterBurns, Character homer) {
+            misterBurns.talk();
+            homer.talk();
+            misterBurns = null;
+            homer = null;
+        }
+    }
+
+    class Character {
+
+        private String talk;
+
+        public Character(String talk) {
+            this.talk = talk;
+        }
+
+        public void talk() {
+            System.out.println(this.talk);
+        }
+    }
+}
