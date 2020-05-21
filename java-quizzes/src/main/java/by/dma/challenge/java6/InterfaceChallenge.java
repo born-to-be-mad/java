1302fb6defaff8c6efab1ff407d74ad4f2aa0f05
Dzmitry Marudau <vinmaster@tut.by>
2020-05-21 11:04:26 +0300

    Quiz on Interfaces: to use key methods with the new interface features.

--- java-quizzes/src/main/java/by/dma/challenge/java6/InterfaceChallenge.java
+++ java-quizzes/src/main/java/by/dma/challenge/java6/InterfaceChallenge.java
@@ -1,8 +1,9 @@
-package by.dma.challenge;
+package by.dma.challenge.java6;
 
 import java.util.function.Function;
 
 /**
+ * Quiz on Interfaces: to use key methods with the new interface features.
  * @author dzmitry.marudau
  * @since 2019.08
  */
@@ -30,11 +31,13 @@ public class InterfaceChallenge {
     }
 
     public static void main(String... starWars) {
-        System.out.println(new Jedi() {
-            public String useForce() {
-                return "X";
+        System.out.println(
+            new Jedi() {
+                public String useForce() {
+                    return "X";
+                }
             }
-        }.attack() + Jedi.useSaber() + Jedi.MASTER);
+            .attack() + Jedi.useSaber() + Jedi.MASTER);
         // first instruction returns 'S'+'Luke'+'F' where 'Luke' is delimiter between 'S', 'F' passed as the parameters
         // Jedi.useSaber() returns 'S'
         // Jedi.MASTER returns 'Yoda'
