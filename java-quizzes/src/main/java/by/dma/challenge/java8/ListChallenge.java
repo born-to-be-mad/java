c5024565915b8fceb28a0d87d74c477cc2675d90
Dzmitry Marudau <vinmaster@tut.by>
2020-08-02 16:31:06 +0300

    Quiz on Collections

--- java-quizzes/src/main/java/by/dma/challenge/java8/ListChallenge.java
+++ java-quizzes/src/main/java/by/dma/challenge/java8/ListChallenge.java
@@ -1,10 +1,12 @@
-package by.dma.challenge;
+package by.dma.challenge.java8;
 
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * Quiz on Collections.
+ * Arrays are objects in Java. Variables store references instead of the object.
+ * When we pass an object reference to a method we are changing the object that is in the heap of the memory.
  *
  * @author dzmitry.marudau
  * @since 2020.5
