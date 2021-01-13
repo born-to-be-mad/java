b51d6476d6a68c93ea3f7049eef0d5ab0f174cb2
Dzmitry Marudau <vinmaster@tut.by>
2021-01-13 18:56:56 +0300

    Collect words from text lines.

--- how-to/src/main/java/by/dma/files/FileUtils.java
+++ how-to/src/main/java/by/dma/files/FileUtils.java
@@ -8,6 +8,14 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.joining;
 
 /**
  * @author dzmitry.marudau
@@ -15,8 +23,7 @@ import java.nio.file.Files;
  */
 public class FileUtils {
     public static void copyFileViaFileStreams(File source, File dest) throws IOException {
-        try (InputStream input = new FileInputStream(source);
-             OutputStream output = new FileOutputStream(dest)) {
+        try (InputStream input = new FileInputStream(source); OutputStream output = new FileOutputStream(dest)) {
             byte[] buf = new byte[1024];
             int bytesRead;
             while ((bytesRead = input.read(buf)) > 0) {
@@ -26,8 +33,8 @@ public class FileUtils {
     }
 
     public static void copyFileViaFileChannels(File source, File dest) throws IOException {
-        try (FileChannel inputChannel = new FileInputStream(source).getChannel();
-             FileChannel outputChannel = new FileOutputStream(dest).getChannel()) {
+        try (FileChannel inputChannel = new FileInputStream(source)
+                .getChannel(); FileChannel outputChannel = new FileOutputStream(dest).getChannel()) {
             outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
         }
     }
@@ -36,9 +43,22 @@ public class FileUtils {
         org.apache.commons.io.FileUtils.copyFile(source, dest);
     }
 
-    public static void copyFileViaJava7Files(File source, File dest)
-        throws IOException {
+    public static void copyFileViaJava7Files(File source, File dest) throws IOException {
         Files.copy(source.toPath(), dest.toPath());
     }
 
+    /**
+     * Split lines of text into separate words,
+     * filter out dumb words to keep everything clean,
+     * group words by the first letter, count how many words are in each group.
+     */
+    public static Map<Character, Long> collectWords(List<String> strings) {
+        return strings.parallelStream()
+                      .flatMap(line -> Stream.of(line.split("\\s+")))
+                      .filter(word -> !"dumb".equalsIgnoreCase(word))
+                      .collect(groupingBy(word -> word.charAt(0), counting()))
+                      .entrySet()
+                      .parallelStream()
+                      .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 }
