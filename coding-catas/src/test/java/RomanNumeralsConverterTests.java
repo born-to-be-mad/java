3af276415d84398750976e4cae07bf5a5bc8ef82
Dzmitry Marudau <vinmaster@tut.by>
2021-01-19 00:07:28 +0300

    TDD Roman -> Arabic: step1.

---
+++ coding-catas/src/test/java/RomanNumeralsConverterTests.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Test for {@link RomanNumeralsConverter}, which converts Roman numerals to Arabic numerals.
+ *
+ * @author dzmitry.marudau
+ * @since 2020.4
+ */
+public class RomanNumeralsConverterTests {
+
+    @Test
+    public void isJUnitWorking() {
+        assertTrue(true);
+    }
+
+}
