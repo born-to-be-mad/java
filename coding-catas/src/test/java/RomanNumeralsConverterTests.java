7b089257f159f8dd2dc6d1064bbb3ea941580dfe
Dzmitry Marudau <vinmaster@tut.by>
2021-01-19 00:21:43 +0300

    TDD Roman -> Arabic: step4.1: tests for all new single-digit Roman numerals cases

--- coding-catas/src/test/java/RomanNumeralsConverterTests.java
+++ coding-catas/src/test/java/RomanNumeralsConverterTests.java
@@ -19,10 +19,12 @@ public class RomanNumeralsConverterTests {
     @Test
     public void convertsSingleRomanDigit() {
         assertEquals(1, RomanNumeralsConverter.convert("I"));
-
         assertEquals(5, RomanNumeralsConverter.convert("V"));
-
         assertEquals(10, RomanNumeralsConverter.convert("X"));
+        assertEquals(50, RomanNumeralsConverter.convert("L"));
+        assertEquals(100, RomanNumeralsConverter.convert("C"));
+        assertEquals(500, RomanNumeralsConverter.convert("D"));
+        assertEquals(1_000, RomanNumeralsConverter.convert("M"));
     }
 
 }
