68e2f71636e22bfefc1c02d4db9f8ddf365ed13d
Dzmitry Marudau <vinmaster@tut.by>
2021-01-19 15:42:04 +0300

    TDD GildedRose - step3: add basic Pin-Down tests

--- coding-catas/src/test/java/by/dma/gildedrose/GildedRoseTest.java
+++ coding-catas/src/test/java/by/dma/gildedrose/GildedRoseTest.java
@@ -2,7 +2,7 @@ package by.dma.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Initial test for legacy {@link GildedRose}.
@@ -13,11 +13,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class GildedRoseTest {
 
     @Test
-    void initialTest() {
-        Item dummyItem = new Item("dummy", 0, 0);
-        var app = new GildedRose(new Item[] {dummyItem});
-        app.updateQuality();
-        assertEquals("dummy", app.items[0].name);
+    void systemLowersValues() {
+        Item item = createAndUpdateItem(15, 25);
+        assertEquals(14, item.sellIn);
+        assertEquals(24, item.quality);
     }
 
+    @Test
+    void qualityDegradesTwiceAsFast() {
+        Item item = createAndUpdateItem(0, 17);
+        assertEquals(15, item.quality);
+    }
+
+    @Test
+    void qualityIsNeverNegative() {
+        Item item = createAndUpdateItem(0, 0);
+        assertEquals(0, item.quality);
+    }
+
+    private Item createAndUpdateItem(int i, int i2) {
+        Item[] items = new Item[] {new Item("foo", i, i2)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return app.items[0];
+    }
 }
