6a7fadd9dcb69a1ef4f3b4fc0e7cef035b63e43e
Dzmitry Marudau <vinmaster@tut.by>
2021-10-13 01:17:00 +0300

    ThreadSafeBigIntegerSequenceGenerator: minor update

--- algorithms/src/main/java/by/dma/interview/ThreadSafeBigIntegerSequenceGenerator.java
+++ algorithms/src/main/java/by/dma/interview/ThreadSafeBigIntegerSequenceGenerator.java
@@ -2,12 +2,14 @@ package by.dma.interview;
 
 import java.math.BigInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
 /**
  * Thread safe BigInteger sequence generator with non-blocking method next(): [1, 2, 4, 8, 16, ...].
  */
 public class ThreadSafeBigIntegerSequenceGenerator {
-    private AtomicReference<BigInteger> current = new AtomicReference<>(null);
+
+    private final AtomicReference<BigInteger> current = new AtomicReference<>(null);
 
     public BigInteger next() {
         BigInteger recent;
@@ -15,7 +17,13 @@ public class ThreadSafeBigIntegerSequenceGenerator {
         do {
             recent = current.get();
             next = (recent == null) ? BigInteger.valueOf(1) : recent.shiftLeft(1);
-        } while (!current.compareAndSet(recent, next));
+        }
+        while (!current.compareAndSet(recent, next));
         return next;
+    }
+
+    public static void main(String[] args) {
+        ThreadSafeBigIntegerSequenceGenerator app = new ThreadSafeBigIntegerSequenceGenerator();
+        Stream.generate(app::next).limit(10).forEach(System.out::println);
     }
 }
